2da1ec512748d22c1ce8223fdb7eba8333adcf04
nsdfxela <nsdfxela@gmail.com>
2022-04-18 10:18:05 +0300

    small-upd

--- l1/cache.hpp
+++ l1/cache.hpp
@@ -1,19 +1,27 @@
-#include <map>
+#include <unordered_map>
 
 
 template <typename K, typename V>
 class LfuCache {
 public:
-
-class FreqEntry {
-public:
-	//unique_ptr<K> key;
-	int count = 0;
+struct DataItem {
+	V value;
+	int counter = 0;
 };
-
+private:
+std::unordered_map<K, DataItem> data;
+int size = 0;
+int smallestCount = -1;
+public:
+LfuCache(int sz) : size{sz} {}
 
 template <typename F>
 V findOrAdd(K key, F addFunc) {
+	auto hit = data.find(key);
+	if(hit!=data.end()) {
+                
+		return hit->second.V;
+	}
 	return addFunc(key);
 }
 
