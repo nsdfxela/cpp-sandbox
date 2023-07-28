4ba34713bc87c8ef47e48947b806ff3d597c4ef8
nsdfxela nsdfxela <alexander.fedosin@itseez3d.com>
2023-07-28 09:09:45 +0300

    fru

--- l1/t1/arc/arc.hpp
+++ l1/t1/arc/arc.hpp
@@ -4,6 +4,7 @@
 #include <cassert>
 #include <unordered_map>
 #include <sstream>
+#include <map>
 
 namespace arc {
   template<typename K, typename V>
@@ -43,6 +44,7 @@ namespace arc {
     void put(K key, V val){
       assert(!contains(key));
       if(cache.size() == capacity){
+	map.erase(cache.back().first);
 	cache.pop_back();
       }
       cache.emplace_front(key, val);
@@ -59,8 +61,51 @@ namespace arc {
   template<typename K, typename V>
   struct lfu
   {
+    int capacity;
+    using entry = typename std::pair<K, V>;
+    using entryFreq = std::pair<int, entry>;
+    using entryIt = typename std::list<entryFreq>::iterator;
+    std::unordered_map<K, entryIt> map;
+    std::list<entryFreq> cache;
+    bool contains(K key){
+      return (map.find(key) != map.end());
+    }
+    void put(K key, V val)
+    {
+      assert(!contains(key));
+      if(cache.size() == capacity){
+	map.erase(cache.back().first);
+	cache.pop_back();
+      }
 
-  }
+      cache.emplace_back(0, std::make_pair(key, val));
+      auto lastElementIt = std::prev(cache.end());
+      map.emplace(key, lastElementIt);
+    }
+    V get(K key)
+    {
+      assert(contains(key));
+      auto eit = map.find(key)->second; //entryIt
+      eit->first += 1; //frequency++
+      if(eit != cache.begin()){
+	auto prevIt = std::prev(eit);
+	if(prevIt->first < eit->first){ //eit met more frequently than the neighbor, lets move it
+	  cache.splice(prevIt, cache, eit);
+	}
+      }
+      
+      return eit->second.second;
+    }
+    lfu(int cap) : capacity{cap} { }
+    std::string toString(){
+      std::stringstream ss;
+      for(auto it = cache.begin(); it != cache.end(); it++){
+	ss << "k:" << it->second.first << " f:" << it->first << ", ";
+      }
+      ss << std::endl;
+      return ss.str();
+    }
+  };
 
 
   template<typename K, typename V>
--- l1/t1/arc/main.cc
+++ l1/t1/arc/main.cc
@@ -5,9 +5,38 @@
 int main(){
 
   //arc::lru<int, int> lru(3);
-  arc::arc<int, int> cache(4);
-  
-   
+  //arc::arc<int, int> cache(4);
+  arc::lfu<int, int> cache(5);
+  cache.put(1,42);
+  cache.put(2,42);
+  cache.put(3,42);
+  cache.put(4,42);
+  cache.put(5,42);
   std::cout << cache.toString();
+  cache.get(1);
+  cache.get(1);
+  cache.get(1);
+  cache.get(1);
+  cache.get(2);
+  cache.get(2);
+  std::cout << cache.toString();
+  cache.get(5);
+  cache.get(5);
+  cache.get(5);
+  cache.get(5);
+  cache.get(5);
+  std::cout << cache.toString();
+  cache.put(7,42);
+  cache.get(7);
+  cache.get(7);
+  cache.get(7);
+  cache.get(7);
+  cache.get(7);
+  cache.get(7);
+
+  std::cout << cache.toString();
+    /*cache.put(1,42);
+  std::cout << cache.toString();*/
+  
   return 0;
 }
