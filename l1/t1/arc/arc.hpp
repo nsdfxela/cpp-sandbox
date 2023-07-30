e53ca5e467f4e335e24c13ad19d61ca0ccc203dc
nsdfxela nsdfxela <alexander.fedosin@itseez3d.com>
2023-07-30 14:20:44 +0300

    some-arc-test

--- l1/t1/arc/arc.hpp
+++ l1/t1/arc/arc.hpp
@@ -15,8 +15,12 @@ namespace arc {
     std::list<std::pair<K,V>> cache;
     int capacity;
     int size() const{
-      return capacity;
+      return cache.size();
     }
+    bool isFull() const {
+      return size() == capacity;
+    }
+
     lru(int cap):capacity{cap}
     {
       
@@ -33,7 +37,7 @@ namespace arc {
       }
     }
   public:
-    std::string toString(){
+      std::string toString(){
       std::stringstream sstr;
       for(auto it = cache.begin(); it != cache.end(); ++it){
 	sstr << it->first << " ";
@@ -44,8 +48,7 @@ namespace arc {
     void put(K key, V val){
       assert(!contains(key));
       if(cache.size() == capacity){
-	map.erase(cache.back().first);
-	cache.pop_back();
+	evict();
       }
       cache.emplace_front(key, val);
       map.emplace(key, cache.begin());
@@ -56,6 +59,32 @@ namespace arc {
       moveFront(key);
       return map.find(key)->second->second;
     }
+    std::pair<K,V> evict(){
+      assert(cache.size());
+      auto result = cache.back();
+      map.erase(cache.back().first);      
+      cache.pop_back();      
+      return result;
+    }
+    std::pair<K,V> evict(K key){
+      auto found = map.find(key);
+      assert(found != map.end());
+      auto result = *(found->second);
+      map.erase(result.first);      
+      cache.erase(found->second);      
+      return result;
+    }
+    void decCapacity(){
+
+      if(capacity > 0){
+	capacity -= 1;
+      }
+      assert(size() <= capacity);
+    }
+    void incCapacity(){
+      capacity+=1;
+    }
+    int getCapacity() const {return capacity;}
   };
 
   template<typename K, typename V>
@@ -67,9 +96,25 @@ namespace arc {
     using entryIt = typename std::list<entryFreq>::iterator;
     std::unordered_map<K, entryIt> map;
     std::list<entryFreq> cache;
+    int size() const {
+      return cache.size();
+    }
+    bool isFull() const {
+      return size() == capacity;
+    }
     bool contains(K key){
       return (map.find(key) != map.end());
     }
+    void decCapacity(){
+      if(capacity > 0){
+	capacity -= 1;
+      }
+      assert(size() <= capacity);
+    }
+    void incCapacity(){
+      capacity+=1;
+    }
+    int getCapacity() const {return capacity;}    
     void put(K key, V val)
     {
       assert(!contains(key));
@@ -96,6 +141,22 @@ namespace arc {
       
       return eit->second.second;
     }
+    std::pair<K,V> evict(){
+      assert(cache.size());
+      auto result = cache.back().second;
+      map.erase(cache.back().first);      
+      cache.pop_back();      
+      return result;
+    }
+    std::pair<K,V> evict(K key){
+      auto found = map.find(key);
+      assert(found != map.end());
+      auto result = *(found->second);
+      map.erase(result.first);      
+      cache.erase(found->second);      
+      return result;
+    }
+
     lfu(int cap) : capacity{cap} { }
     std::string toString(){
       std::stringstream ss;
@@ -113,7 +174,8 @@ namespace arc {
   {
     int capacity;
     //int t1_cap, t2_cap;
-    lru<K,V> t1, t2;
+    lru<K,V> t1;
+    lfu<K,V> t2;
     lru<K,int> b1, b2;
     arc(int cap) : capacity{cap},
 		   t1{cap/2},
@@ -121,16 +183,67 @@ namespace arc {
 		   b1{cap/2},
 		   b2{cap-cap/2}
     {
-            
+      
     }
-
-    
+    std::pair<K,V> evictT2(){
+      auto evictedElement = t2.evict();
+      if(b2.contains(evictedElement.first)) {
+	b2.get(evictedElement.first);
+      } else {
+	b2.put(evictedElement.first, 0);
+      }
+      return evictedElement;
+    }
+    std::pair<K,V> evictT1(){
+      auto evictedElement = t1.evict();
+      if(b1.contains(evictedElement.first)) {
+	  b1.get(evictedElement.first);
+      } else {
+	  b1.put(evictedElement.first, 0);
+      }
+      return evictedElement;
+    }
+    template<typename F>
+    V get(K key, F functionToGet){
+      if(t1.contains(key)){ //Found in T1, move it to T2
+	auto element = t1.evict(key);
+	if(t2.isFull()){
+	  evictT2();	  
+	}	
+	t2.put(element.first, element.second);
+	return element.second;
+      } else if (t2.contains(key)){ //Found in T2
+	return t2.get(key);
+      }
+      //Not found in T1 and T2
+      if (b2.contains(key) && t1.size()) { 
+	evictT1();
+	t1.decCapacity();
+	if(t2.getCapacity() < capacity){
+	  t2.incCapacity();
+	}
+      } else if (b1.contains(key) && t2.size()) {
+	evictT2();
+	t2.decCapacity();
+	if(t1.getCapacity() < capacity){
+	  t1.incCapacity();
+	}
+      }
+      auto val = functionToGet(key);
+      if(t1.isFull()){
+	evictT1();
+      }
+      t1.put(key, val);
+      return val;
+    }
     
     std::string toString(){
       std::stringstream ss;
 
       ss << "t1 size " << t1.size() << ":" << t1.toString();
       ss << "t2 size " << t2.size() << ":" << t2.toString();
+      ss << "b1: " << b1.toString();
+      ss << "b2: " << b2.toString();
       return ss.str();
     }
     
--- l1/t1/arc/main.cc
+++ l1/t1/arc/main.cc
@@ -2,11 +2,46 @@
 #include "arc.hpp"
 
 
+int slowGetFunction(int key){
+  std::cout << "Slow get function called for key " << key << ". Cache miss \n";
+  return 42;
+}
+		  
+
 int main(){
 
+  arc::arc <int, int> arc(6);
+  arc.get(3, slowGetFunction);
+  arc.get(3, slowGetFunction);
+  arc.get(3, slowGetFunction);
+  arc.get(3, slowGetFunction);
+
+  arc.get(12, slowGetFunction);
+  arc.get(1, slowGetFunction);
+  arc.get(5, slowGetFunction);
+  arc.get(4, slowGetFunction);
+
+  arc.get(5, slowGetFunction);
+  arc.get(4, slowGetFunction);
+
+  arc.get(100, slowGetFunction);
+  arc.get(101, slowGetFunction);
+
+  arc.get(12, slowGetFunction);
+  arc.get(100, slowGetFunction);
+  
+  arc.get(14, slowGetFunction);
+  arc.get(13, slowGetFunction);
+  arc.get(150, slowGetFunction);
+  arc.get(151, slowGetFunction);
+
+  arc.get(12, slowGetFunction);  
+  
+
+  std::cout << arc.toString();
   //arc::lru<int, int> lru(3);
   //arc::arc<int, int> cache(4);
-  arc::lfu<int, int> cache(5);
+  /*arc::lfu<int, int> cache(5);
   cache.put(1,42);
   cache.put(2,42);
   cache.put(3,42);
@@ -34,7 +69,7 @@ int main(){
   cache.get(7);
   cache.get(7);
 
-  std::cout << cache.toString();
+  std::cout << cache.toString();*/
     /*cache.put(1,42);
   std::cout << cache.toString();*/
   
